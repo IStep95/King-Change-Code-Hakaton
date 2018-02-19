e4dc9128dffe8c4d7d46414fa452d321f637c455
Ivan Stepanic <stepanic.ivan@gmail.com>
2018-02-19 22:15:53 +0100

    loader added to modals, validation implemented in js for login

--- HackIt2018Template/HackIt2018Template/wwwroot/js/loginModalHandler.js
+++ HackIt2018Template/HackIt2018Template/wwwroot/js/loginModalHandler.js
@@ -1,4 +1,5 @@
 ﻿var modal2;
+var isErrorTxtCurrentlyDisplayed;
 
 
 function DisplayLoginModalProcedure() {
@@ -15,30 +16,98 @@ function DisplayLoginModalProcedure() {
         }
     }
 
+    StopLoader();
+
 }
+
+
+function DisplayErrorMessage(message)
+{
+    $("#errorMessageHolder").text(message);
+    isErrorTxtCurrentlyDisplayed = true;
+    $("#errorMessageHolder").fadeIn(1500).delay(500).fadeOut(1500, function () {
+        // Animation complete.
+        isErrorTxtCurrentlyDisplayed = false;
+    });
+}
+
+
+function ShowLoader()
+{
+    $("#loginFormLoader").show();
+}
+
+
+function StopLoader()
+{
+    $("#loginFormLoader").hide();
+}
+
+
 
 ///should get called when user presses login btn on login form. This calls the method for login in home controller
 function LoginUser()
 {
-    var loginData = GetLoginFormData();
+    if (ValidateLoginForm()) {
 
-    $.ajax({
-        type: 'POST',
-        url: 'Home/Login',
-        contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(loginData),
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            alert("prijava uspješna! :)");
-            modal2.style.display = "none";
 
-        },
-        error: function (xhr, status, error) {
-            alert(xhr.responseText);
+        ShowLoader();
+
+        var delayInMilliseconds = 300;
+
+        setTimeout(function () {
+
+            var loginData = GetLoginFormData();
+
+            $.ajax({
+                type: 'POST',
+                url: 'Home/Login',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(loginData),
+                dataType: 'json',
+                async: false,
+                success: function (data) {
+                    StopLoader();
+                    alert("prijava uspješna! :)");
+                    modal2.style.display = "none";
+                },
+                error: function (xhr, status, error) {
+                    StopLoader();
+                    if (!isErrorTxtCurrentlyDisplayed) {
+                        DisplayErrorMessage(xhr.responseText.replace('[', '').replace("]", "").replace("\"", "").replace("!\"", ""));
+                    }
+                }
+            });
+
+        }, delayInMilliseconds);
+    } 
+}
+
+
+function ValidateLoginForm()
+{
+    var teamData = GetLoginFormData();
+    if (teamData.Teamname.trim().length === 0 )
+    {
+        //alert("niste unjeli ime tima");
+        if (!isErrorTxtCurrentlyDisplayed)
+        {
+            DisplayErrorMessage("niste unjeli ime tima");
         }
-
-    });
+        return false;
+    }
+    else if (teamData.Password.trim().length === 0)
+    {
+        if (!isErrorTxtCurrentlyDisplayed)
+        {
+            DisplayErrorMessage("niste unjeli lozinku");
+        }
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 
