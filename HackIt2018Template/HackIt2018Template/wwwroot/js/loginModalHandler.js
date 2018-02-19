b2c815402973d07b5ff7d6958ab4b47d1f3711e7
Ivan Stepanic <stepanic.ivan@gmail.com>
2018-02-19 19:27:53 +0100

    modals improved

---
+++ HackIt2018Template/HackIt2018Template/wwwroot/js/loginModalHandler.js
@@ -0,0 +1,57 @@
+﻿
+function DisplayLoginModalProcedure() {
+
+    document.getElementById('id02').style.display = 'block';
+
+    // Get the login modal
+    var modal2 = document.getElementById('id02');
+
+    // When the user clicks anywhere outside of the login modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
+
+}
+
+///should get called when user presses login btn on login form. This calls the method for login in home controller
+function LoginUser()
+{
+    var loginData = GetLoginFormData();
+
+    $.ajax({
+        type: 'POST',
+        url: 'Home/Login',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(loginData),
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            alert(data);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+
+    });
+}
+
+
+///login form data ==> same as for registration, except members are null!!
+function GetLoginFormData()
+{
+    var td = {
+        Teamname: document.getElementById("Login_TeamName").value,
+        Password: document.getElementById("Login_password").value,
+    };
+
+    return td;
+}
+
+
+
+
+
+
+
---
+++ HackIt2018Template/HackIt2018Template/wwwroot/js/registrationModalHandler.js
@@ -0,0 +1,76 @@
+﻿//used for team registration on modal window, home
+
+
+function DisplayRegisterModalProcedure()
+{
+
+    document.getElementById('id01').style.display = 'block';
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+}
+
+
+
+//ajax for sending form data to the controller !
+function SendFormData()
+{
+    var teamData = GetTeamData();
+
+    $.ajax({
+        type: 'POST',
+        url: 'Home/Register',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(teamData),
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            alert(data);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+        }
+
+    });
+}
+
+function GetTeamData()
+{
+    var td = {
+        Teamname: document.getElementById("TeamName").value,
+        Password: document.getElementById("TeamPassword").value,
+        Members: GetMembers()
+    };
+
+    return td;
+}
+
+//returns array of all 4 members in form provided
+function GetMembers()
+{
+    //name, surname, mail
+    var members = [];
+
+    for (var i = 1; i <= 4; i++)
+    {
+        var memberNameId = "member" + i.toString() + "_name";
+        var memberSurnameId = "member" + i.toString() + "_surname";
+        var memberMailId = "member" + i.toString() + "_mail";
+
+        members.push
+        ({
+            name: document.getElementById(memberNameId).value,
+            surname: document.getElementById(memberSurnameId).value,
+            mail: document.getElementById(memberMailId).value,
+        })
+    }
+    return members; 
+}
+
